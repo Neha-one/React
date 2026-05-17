27db8585fba18c3f300dcce8efd8c0ba71ae5c1e
Neha-one <justforgtb@gmail.com>
2026-05-17 18:24:25 +0530

    add these files

---
+++ react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,10 @@
+import {KgButton} from "./KgButton";
+function app(){
+  return(
+<div>     
+   <h1>Hii</h1>
+<KgButton></KgButton>
+</div>
+  )
+}
+export default app;
---
+++ react-comp/create-first-comp/src/kgButton.jsx
@@ -0,0 +1,6 @@
+export function KgButton(){
+  return(
+    <button>click here for info</button>
+  )
+}
+// export default KgButton;
