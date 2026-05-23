2c9b2bf7067375a9535cd327761e8f8b309bcb66
Neha-one <justforgtb@gmail.com>
2026-05-23 18:01:17 +0530

    add these files

--- 07-PassingDataViaProps/learning-props/src/components/Item.jsx
+++ 07-PassingDataViaProps/learning-props/src/components/Item.jsx
@@ -6,15 +6,33 @@ const Item = ({ itemName }) => {
   return (
     <>
 
-      <li className={`${styles['kg-item']} `} >       {/*method-01 to apply css module. */}
-        {/* <li className={style.container} >     method-01 to apply css module. and here container is use to as example there is no contaiener className. */}
-
-
-        <span className={styles['kg-span']}>        {/* method-02 to apply css module. */}
-          {itemName}</span>
+      <li className={`${styles['kg-item']} `} >
+        <span className={styles['kg-span']}>  {itemName}</span>
       </li>
 
     </>
   )
 }
 export default Item;
+
+
+
+// //here styles is an object we can use any other name like css .
+// import styles from "../cssModule/Item.module.css";
+
+// const Item = ({ itemName }) => {
+
+//   return (
+//     <>
+
+//       <li className={`${styles['kg-item']} `} >       {/*method-01 to apply css module. */}
+//         {/* <li className={style.container} >     method-01 to apply css module. and here container is use to as example there is no contaiener className. */}
+
+
+//         <span className={styles['kg-span']}>        {/* method-02 to apply css module. */}
+//           {itemName}</span>
+//       </li>
+
+//     </>
+//   )
+// }
